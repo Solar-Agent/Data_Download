2170b47e2b3fc009cea0e4a9a2e016294f9ddcb1
sooyoung-wind <sooyoung.wind@gmail.com>
2025-04-22 07:21:23 +0900

    ✨ feat: Add fun. "find missing dates"

--- Data_Download/utility.py
+++ Data_Download/utility.py
@@ -19,5 +19,42 @@ def generate_month_list(start_date: str, end_date: str) -> list:
 
     return date_list
 
+def find_missing_dates(
+    df: pd.DataFrame,
+    date_column: str,
+    freq: str='h',
+    verbose: bool=True
+) -> list:
+    """
+    특정 시간 범위 내에서 누락된 날짜/시간을 찾는 함수.
+
+    Args:
+        df (pd.DataFrame): 날짜/시간 데이터가 있는 DataFrame
+        date_column (str): 날짜/시간 데이터가 있는 컬럼 이름
+        freq (str): 시간 간격 ('h' - 1시간, 'D' - 1일 등)
+
+    Returns:
+        list: 누락된 날짜/시간 리스트
+    """
+    # 문자열 형식의 날짜를 datetime 형식으로 변환
+    df[date_column] = pd.to_datetime(df[date_column])
+    df = df.sort_values(by=date_column)
+
+    full_range = pd.date_range(start=df[date_column].min(), end=df[date_column].max(), freq=freq)
+    missing_dates = full_range.difference(df[date_column]).tolist()
+
+    if verbose:
+        print(f"missing dates: {missing_dates}")
+        print(f"Total missing dates: {len(missing_dates)}")
+
+    return missing_dates
+
+
 if __name__ == "__main__":
     print(generate_month_list("2022-10", "2023-02"))
+    # 사용 예시:
+    data = {'dates': ['2022-01-01 01:00:00', '2022-01-03 03:00:00']}
+    df = pd.DataFrame(data)
+    # 1시간 단위로 누락된 시간 찾기
+    missing_hourly = find_missing_dates(df, 'dates', freq='h')
+
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from Data_Download.merge_era5 import download_and_merge_era5_solar
+from Data_Download.utility import find_missing_dates
 
+# 전체 프로세스
 df = download_and_merge_era5_solar(
     start_date="2014-12",
     end_date="2023-01",
     lat=37.257,
     lon=126.983
 )
+
+# 중간에 빠진 시간 데이터가 있는지 확인
+find_missing_dates(df, "valid_time_kst", freq='h')
+
+# 저장
 df.to_csv("data.csv", index=False)
 print(df.head())
